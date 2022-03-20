95c285055e0a58d676d3d05c6d26271ab8d520a4
capmahin <mahinulhuque1996@gmail.com>
2022-03-21 05:08:50 +0600

    add in json

--- src/Components/Cosmatics/Cosmatics.js
+++ src/Components/Cosmatics/Cosmatics.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Cosmetic from '../Cosmetic/Cosmetic';
 // import Cosmatic from '../cosmatic/cosmatic';
@@ -6,43 +6,15 @@ import Cosmetic from '../Cosmetic/Cosmetic';
 ;
 
 const Cosmatics = () => {
-    const cosmatics = [
-        {
-          "id": "6237b13ca23cc7707ec351d8",
-          "price": 278,
-          "name": "Vargas Newton"
-        },
-        {
-          "id": "6237b13c3bb226a898ae430b",
-          "price": 216,
-          "name": "Vera Rollins"
-        },
-        {
-          "id": "6237b13c704852097787624c",
-          "price": 67,
-          "name": "Caitlin Bishop"
-        },
-        {
-          "id": "6237b13cc43c6dd85d031298",
-          "price": 419,
-          "name": "Kathy Mcneil"
-        },
-        {
-          "id": "6237b13c1ecca6ab78011fab",
-          "price": 486,
-          "name": "Snider Blake"
-        },
-        {
-          "id": "6237b13cd0009e996c7ab49e",
-          "price": 210,
-          "name": "Penny Holland"
-        },
-        {
-          "id": "6237b13c37a7769a6c1066b3",
-          "price": 439,
-          "name": "Randolph Kline"
-        }
-      ]
+    const [ cosmatics, setcosmatics] = useState([]);
+
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setcosmatics(data))
+
+
+    },[])
     return (
         <div>
             <h1>This is my cosmatic website
