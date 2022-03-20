dd0349a3334911f9547c413fb03c1389a12ee9be
capmahin <mahinulhuque1996@gmail.com>
2022-03-21 04:57:34 +0600

    add new data

--- src/Components/Cosmatics/Cosmatics.js
+++ src/Components/Cosmatics/Cosmatics.js
@@ -7,12 +7,42 @@ import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmatics = () => {
     const cosmatics = [
-        {id:1, name:'alta', price:100},
-        {id:2, name:'palis', price:200},
-        {id:3, name:'malis', price:300},
-        {id:4, name:'balis', price:400},
-        {id:5, name:'nalis', price:500},
-    ]
+        {
+          "id": "6237b13ca23cc7707ec351d8",
+          "price": 278,
+          "name": "Vargas Newton"
+        },
+        {
+          "id": "6237b13c3bb226a898ae430b",
+          "price": 216,
+          "name": "Vera Rollins"
+        },
+        {
+          "id": "6237b13c704852097787624c",
+          "price": 67,
+          "name": "Caitlin Bishop"
+        },
+        {
+          "id": "6237b13cc43c6dd85d031298",
+          "price": 419,
+          "name": "Kathy Mcneil"
+        },
+        {
+          "id": "6237b13c1ecca6ab78011fab",
+          "price": 486,
+          "name": "Snider Blake"
+        },
+        {
+          "id": "6237b13cd0009e996c7ab49e",
+          "price": 210,
+          "name": "Penny Holland"
+        },
+        {
+          "id": "6237b13c37a7769a6c1066b3",
+          "price": 439,
+          "name": "Randolph Kline"
+        }
+      ]
     return (
         <div>
             <h1>This is my cosmatic website
