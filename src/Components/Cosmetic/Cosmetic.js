c8d492ab7846654af23b0a6abc82eb1a194aab0f
capmahin <mahinulhuque1996@gmail.com>
2022-03-21 05:29:10 +0600

    add perameter in onClick

--- src/Components/Cosmetic/Cosmetic.js
+++ src/Components/Cosmetic/Cosmetic.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name , price , id} = props.cosmatic;
+    const addToCart =(id)=>{
+        console.log('item added' , id)
+    }
     return (
-        <div>
+        <div className='product'>
             <h3>Buy Now:{name}</h3>
             <p>Only for:${price}</p>
             <p><small>its has id:{id}</small></p>
+            <button onClick={()=>addToCart(id)}>Add to cart</button>
+            <button onClick={()=>addToCart(id)}>Add to Purchase</button>
         </div>
     );
 };
