96271dba9d626617854c718aec9cb7ff1e126fb6
capmahin <mahinulhuque1996@gmail.com>
2022-03-21 16:43:48 +0600

    declare data as object and conver it in string with json.stringify

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,13 +1,15 @@
 const addToDb =id =>{
+    let shoppingCart ={};
+    localStorage.setItem('shoppingCart',JSON.stringify(shoppingCart));
     const quantity = localStorage.getItem(id);
     if(quantity){
-        console.log('already exist');
+        
         const newQuanity = parseInt(quantity) + 1;
-        localStorage.setItem(id , newQuanity);
+        // localStorage.setItem(id , newQuanity);
     }
     else{
         console.log('new item')
-        localStorage.setItem(id ,1);
+        // localStorage.setItem(id ,1);
 
     }
     
