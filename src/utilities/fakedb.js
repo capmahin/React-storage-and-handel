191e6c52edc93f9bf63891f054f3daceb36c4af5
capmahin <mahinulhuque1996@gmail.com>
2022-03-21 16:59:17 +0600

    add storedCard

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,17 +1,27 @@
 const addToDb =id =>{
-    let shoppingCart ={};
-    localStorage.setItem('shoppingCart',JSON.stringify(shoppingCart));
-    const quantity = localStorage.getItem(id);
+    let shoppingCart;
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
+    }
+    else{
+        shoppingCart={};
+    }
+    
+    const quantity = shoppingCart[id];
     if(quantity){
         
-        const newQuanity = parseInt(quantity) + 1;
+        const newQuanity = quantity + 1;
+        shoppingCart[id] = newQuanity;
         // localStorage.setItem(id , newQuanity);
     }
     else{
-        console.log('new item')
+        shoppingCart[id] = 1;
         // localStorage.setItem(id ,1);
 
     }
+    localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart)
+    );
     
 }
 export{addToDb}
